efdf3539b1743f3ec1964529081d368908cd58d8
DuWei <duweiwork@gmail.com>
2022-05-19 18:07:58 +0900

    add wemix fields to user

--- plugin.rb
+++ plugin.rb
@@ -14,4 +14,25 @@ register_asset "stylesheets/common.scss"
 register_svg_icon "wallet" if respond_to?(:register_svg_icon)
 
 after_initialize do
+  module ::DiscourseWemix
+    PLUGIN_NAME ||= "discourse_wemix"
+
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace DiscourseWemix
+    end
+
+    class Error < StandardError; end
+  end
+
+  require_relative "app/controllers/discourse_wemix/wemix_controller.rb"
+  require_relative "app/models/discourse_wemix/activity.rb"
+
+  DiscourseWemix::Engine.routes.draw do
+    put "/connect" => 'wemix#connect'
+  end
+
+  Discourse::Application.routes.append do
+    mount ::DiscourseWemix::Engine, at: "/wemix"
+  end
 end
